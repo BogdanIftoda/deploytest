3c82ed8d9362d7e5b24d121d03dd896f9731679c
Bogdan Iftoda <bogdaniftoda@gmail.com>
2025-03-24 08:58:53 +0200

    update docker compose

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -82,29 +82,29 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 #
-# DB_NAME = os.environ.get("DB_NAME", "ecommerce")
-# DB_USER = os.environ.get("DB_USER", "root")
-# DB_PASSWORD = os.environ.get("DB_PASSWORD", "root")
-# DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")
-# DB_PORT = os.environ.get("DB_PORT", "3306")
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": DB_NAME,
-#         "USER": DB_USER,
-#         "PASSWORD": DB_PASSWORD,
-#         "HOST": DB_HOST,
-#         "PORT": DB_PORT,
-#     }
-# }
+DB_NAME = os.environ.get("DB_NAME", "ecommerce")
+DB_USER = os.environ.get("DB_USER", "root")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "root")
+DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")
+DB_PORT = os.environ.get("DB_PORT", "3306")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
